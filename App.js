910c174c98e520812339960c1753d9056bd31b2e
Srinivas Joshi <69837749+SrinivasJoshi@users.noreply.github.com>
2022-01-14 18:37:38 +0530

    responsiveness and img

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 const menuBtn = document.getElementById("menuBtn");
 const cancelBtn = document.getElementById("cancelBtn");
 const navContent = document.getElementById("navContent");
+const navLinks = document.getElementsByClassName("navLinks");
 
 menuBtn.addEventListener("click",()=>{
     menuBtn.classList.add("notVisible");
     cancelBtn.classList.remove("notVisible");
     navContent.classList.remove("notVisible");
 })
-cancelBtn.addEventListener("click",()=>{
+const cancelFun =()=>{
     cancelBtn.classList.add("notVisible");
     navContent.classList.add("notVisible");
     menuBtn.classList.remove("notVisible");
-})
+}
+navLinks[0].addEventListener("click",cancelFun);
+navLinks[1].addEventListener("click",cancelFun);
+navLinks[2].addEventListener("click",cancelFun);
+navLinks[3].addEventListener("click",cancelFun);
+navLinks[4].addEventListener("click",cancelFun);
+cancelBtn.addEventListener("click",cancelFun);
